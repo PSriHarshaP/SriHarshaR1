e96e9392e0ee96dbabe29c632674ff7713b5e18a
Sri Harsha <harshasrip.s@gmail.com>
2025-11-18 20:44:35 +0530

    Adding Srireact1 to this repository

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from './Components/Home.jsx'
+import Page1 from './Components/Page1.jsx'
+function App() {
+
+  return (
+      <div>
+        <Home />
+        <Page1 />
+      </div>  
+  )
+}
+
+export default App
---
+++ src/Components/Page1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export function Page1() {
+  return (
+    <div>
+        <img src="https://cdn1.vectorstock.com/i/1000x1000/57/95/mobile-website-landing-page-template-vector-32395795.jpg" alt="Placeholder Image" />
+        <li>Landing Page</li>
+        <li>Page Website</li>
+        <li>Design</li>
+        <li>Template</li>
+    </div>
+  )
+}
+
+export default Page1
